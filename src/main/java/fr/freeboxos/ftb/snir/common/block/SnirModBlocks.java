c43968c2b2af82d2c063b03124397d19b500726a
Alan700 <ftbmodified700@gmail.com>
2019-11-07 21:33:27 +0100

    Ajout du minerais du nether star dust.
    Ce minerais permet de dropper 1 nether star dust.

--- src/main/java/fr/freeboxos/ftb/snir/common/block/SnirModBlocks.java
+++ src/main/java/fr/freeboxos/ftb/snir/common/block/SnirModBlocks.java
@@ -36,16 +36,34 @@ public class SnirModBlocks {
     @ObjectHolder(SnirMod.MOD_ID + ":block_nugget_diamond")
     public static final Block BLOCK_NUGGET_DIAMOND = null;
 
+    /**
+     * Block dust nether star ore
+     */
+    @ObjectHolder(SnirMod.MOD_ID + ":block_dust_netherstar")
+    public static final Block BLOCK_DUST_NETHERSTAR = null;
+
+    /**
+     * Déclaration de tous les blocs du mod.
+     *
+     * @param event
+     */
     @SubscribeEvent
     public static void registerBlock(final RegistryEvent.Register<Block> event) {
         event.getRegistry().register(new BlockNuggetEmerald(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F)).setRegistryName("block_nugget_emerald"));
         event.getRegistry().register(new BlockNuggetDiamond(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F)).setRegistryName("block_nugget_diamond"));
+        event.getRegistry().register(new BlockDustNetherStar(Block.Properties.create(Material.ROCK).hardnessAndResistance(1.5F, 6.0F)).setRegistryName("block_dust_netherstar"));
     }
 
+    /**
+     * Déclaration de tous les item lier au blocs du mod.
+     *
+     * @param event
+     */
     @SubscribeEvent
     public static void registerItem(final RegistryEvent.Register<Item> event) {
         event.getRegistry().register(new BlockItem(BLOCK_NUGGET_EMERALD, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(BLOCK_NUGGET_EMERALD.getRegistryName()));
         event.getRegistry().register(new BlockItem(BLOCK_NUGGET_DIAMOND, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(BLOCK_NUGGET_DIAMOND.getRegistryName()));
+        event.getRegistry().register(new BlockItem(BLOCK_DUST_NETHERSTAR, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(BLOCK_DUST_NETHERSTAR.getRegistryName()));
     }
 
 }
